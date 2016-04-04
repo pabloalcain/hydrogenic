f63d5c1d60d76a67e59d63ec66d54e2999099b3d
Pablo Alcain <pabloalcain@gmail.com>
2016-04-04 01:39:41 -0300

    Forgot example!

---
+++ hydrogen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+"""
+Tiny example to show how to use the atomic module
+"""
+
+import atomic as at
+import pylab as pl
+
+at.set_potential(-1, 0, 0)
+pl.figure()
+for n, l in zip((1, 2, 2), (0, 0, 1)):
+  E, r, p, q = at.bound(n, l, 'S')
+  print "Energy of Schrödinger for n = {n} and {l} is {E}".format(n=n, l=l, E=E)
+  pl.plot(r, p/r, label='n = {n}, l = {l}'.format(n=n, l=l))
+pl.legend()
+pl.xlabel('r')
+pl.ylabel(r'$\Psi(r)$')
+pl.title('Hydrogen bound states')
+pl.savefig('h_bound.png')
+pl.show()
